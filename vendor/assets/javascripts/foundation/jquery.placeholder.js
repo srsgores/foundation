7028873fb980573e80312287646b353d303a6999
Sean Goresht <seangores@gmail.com>
2012-12-29 21:53:44 -0700

    Reformat code to use tabs
    Reformat code to use tabs instead of spaces
    
    Tabs will be 4 spaces.

--- vendor/assets/javascripts/foundation/jquery.placeholder.js
+++ vendor/assets/javascripts/foundation/jquery.placeholder.js
@@ -1,24 +1,31 @@
 /*! http://mths.be/placeholder v2.0.7 by @mathias */
-;(function(window, document, $) {
+;
+(function (window, document, $)
+{
 
 	var isInputSupported = 'placeholder' in document.createElement('input'),
-	    isTextareaSupported = 'placeholder' in document.createElement('textarea'),
-	    prototype = $.fn,
-	    valHooks = $.valHooks,
-	    hooks,
-	    placeholder;
+		isTextareaSupported = 'placeholder' in document.createElement('textarea'),
+		prototype = $.fn,
+		valHooks = $.valHooks,
+		hooks,
+		placeholder;
 
-	if (isInputSupported && isTextareaSupported) {
+	if (isInputSupported && isTextareaSupported)
+	{
 
-		placeholder = prototype.placeholder = function() {
+		placeholder = prototype.placeholder = function ()
+		{
 			return this;
 		};
 
 		placeholder.input = placeholder.textarea = true;
 
-	} else {
+	}
+	else
+	{
 
-		placeholder = prototype.placeholder = function() {
+		placeholder = prototype.placeholder = function ()
+		{
 			var $this = this;
 			$this
 				.filter((isInputSupported ? 'textarea' : ':input') + '[placeholder]')
@@ -36,25 +43,34 @@
 		placeholder.textarea = isTextareaSupported;
 
 		hooks = {
-			'get': function(element) {
+			'get': function (element)
+			{
 				var $element = $(element);
 				return $element.data('placeholder-enabled') && $element.hasClass('placeholder') ? '' : element.value;
 			},
-			'set': function(element, value) {
+			'set': function (element, value)
+			{
 				var $element = $(element);
-				if (!$element.data('placeholder-enabled')) {
+				if (!$element.data('placeholder-enabled'))
+				{
 					return element.value = value;
 				}
-				if (value == '') {
+				if (value == '')
+				{
 					element.value = value;
 					// Issue #56: Setting the placeholder causes problems if the element continues to have focus.
-					if (element != document.activeElement) {
+					if (element != document.activeElement)
+					{
 						// We can't use `triggerHandler` here because of dummy text/password inputs :(
 						setPlaceholder.call(element);
 					}
-				} else if ($element.hasClass('placeholder')) {
+				}
+				else if ($element.hasClass('placeholder'))
+				{
 					clearPlaceholder.call(element, true, value) || (element.value = value);
-				} else {
+				}
+				else
+				{
 					element.value = value;
 				}
 				// `set` can not return `undefined`; see http://jsapi.info/jquery/1.7.1/val#L2363
@@ -65,50 +81,64 @@
 		isInputSupported || (valHooks.input = hooks);
 		isTextareaSupported || (valHooks.textarea = hooks);
 
-		$(function() {
+		$(function ()
+		{
 			// Look for forms
-			$(document).delegate('form', 'submit.placeholder', function() {
+			$(document).delegate('form', 'submit.placeholder', function ()
+			{
 				// Clear the placeholder values so they don't get submitted
 				var $inputs = $('.placeholder', this).each(clearPlaceholder);
-				setTimeout(function() {
+				setTimeout(function ()
+				{
 					$inputs.each(setPlaceholder);
 				}, 10);
 			});
 		});
 
 		// Clear placeholder values upon page reload
-		$(window).bind('beforeunload.placeholder', function() {
-			$('.placeholder').each(function() {
+		$(window).bind('beforeunload.placeholder', function ()
+		{
+			$('.placeholder').each(function ()
+			{
 				this.value = '';
 			});
 		});
 
 	}
 
-	function args(elem) {
+	function args(elem)
+	{
 		// Return an object of element attributes
 		var newAttrs = {},
-		    rinlinejQuery = /^jQuery\d+$/;
-		$.each(elem.attributes, function(i, attr) {
-			if (attr.specified && !rinlinejQuery.test(attr.name)) {
+			rinlinejQuery = /^jQuery\d+$/;
+		$.each(elem.attributes, function (i, attr)
+		{
+			if (attr.specified && !rinlinejQuery.test(attr.name))
+			{
 				newAttrs[attr.name] = attr.value;
 			}
 		});
 		return newAttrs;
 	}
 
-	function clearPlaceholder(event, value) {
+	function clearPlaceholder(event, value)
+	{
 		var input = this,
-		    $input = $(input);
-		if (input.value == $input.attr('placeholder') && $input.hasClass('placeholder')) {
-			if ($input.data('placeholder-password')) {
+			$input = $(input);
+		if (input.value == $input.attr('placeholder') && $input.hasClass('placeholder'))
+		{
+			if ($input.data('placeholder-password'))
+			{
 				$input = $input.hide().next().show().attr('id', $input.removeAttr('id').data('placeholder-id'));
 				// If `clearPlaceholder` was called from `$.valHooks.input.set`
-				if (event === true) {
+				if (event === true)
+				{
 					return $input[0].value = value;
 				}
 				$input.focus();
-			} else {
+			}
+			else
+			{
 				input.value = '';
 				$input.removeClass('placeholder');
 				input == document.activeElement && input.select();
@@ -116,18 +146,25 @@
 		}
 	}
 
-	function setPlaceholder() {
+	function setPlaceholder()
+	{
 		var $replacement,
-		    input = this,
-		    $input = $(input),
-		    $origInput = $input,
-		    id = this.id;
-		if (input.value == '') {
-			if (input.type == 'password') {
-				if (!$input.data('placeholder-textinput')) {
-					try {
+			input = this,
+			$input = $(input),
+			$origInput = $input,
+			id = this.id;
+		if (input.value == '')
+		{
+			if (input.type == 'password')
+			{
+				if (!$input.data('placeholder-textinput'))
+				{
+					try
+					{
 						$replacement = $input.clone().attr({ 'type': 'text' });
-					} catch(e) {
+					}
+					catch (e)
+					{
 						$replacement = $('<input>').attr($.extend(args(this), { 'type': 'text' }));
 					}
 					$replacement
@@ -149,7 +186,9 @@
 			}
 			$input.addClass('placeholder');
 			$input[0].value = $input.attr('placeholder');
-		} else {
+		}
+		else
+		{
 			$input.removeClass('placeholder');
 		}
 	}
